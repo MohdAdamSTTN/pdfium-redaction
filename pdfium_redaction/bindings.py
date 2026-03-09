d958613e5bd51600a2f8bcd5a1628324cf38f3a2
MohDAdamSTTN <sithuthetnaing@hexcode.tech>
2026-03-09 11:14:42 +0700

    Load bundled PDFium library explicitly

--- pdfium_redaction/bindings.py
+++ pdfium_redaction/bindings.py
@@ -1,6 +1,11 @@
 import ctypes
+import pathlib
 
-_pdfium = ctypes.CDLL(None)
+# locate bundled PDFium library
+lib_path = pathlib.Path(__file__).parent / "libpdfium.dylib"
+
+# load your custom PDFium build
+_pdfium = ctypes.CDLL(str(lib_path))
 
 FPDFTextObj_RemoveChars = _pdfium.FPDFTextObj_RemoveChars
 FPDFTextObj_RemoveChars.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int]
